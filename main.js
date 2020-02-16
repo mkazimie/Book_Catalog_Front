5618d5f0079a63a2e20552d542d81100c71c5586
Magdalena Kazimieruk <mkazimieruk@hotmail.com>
2020-02-16 15:55:47 +0100

    task 3

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 $(function () {
 
-    $.ajax({
-        url: 'https://swapi.co/api/people/4/',
-        method: "GET"
-    }).done(function (result) {
-        console.log(result)
-    });
+    var app = $("#app");
+
+
+    function renderBooks(books) {
+        books.forEach(function (book) {
+            var bookElement = $("<div>");
+            bookElement
+                .addClass("book")
+                .appendTo(app);
+
+            var bookTitle = $("<div>");
+            bookTitle
+                .addClass("book-title")
+                .text(book.title)
+                .appendTo(bookElement);
+
+            var bookDetails = $("<div>");
+            bookDetails
+                .addClass("book-details")
+                .appendTo(bookElement);
+        });
+    }
+
+
+        $.ajax({
+            url: 'http://localhost:8282/books/',
+            method: "GET"
+        }).done(renderBooks);
 
 });
