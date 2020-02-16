8f88e708c1afeac9d119748a61f6139c75a0064a
Magdalena Kazimieruk <mkazimieruk@hotmail.com>
2020-02-16 16:21:48 +0100

    task 4 done

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
-$(function () {
-
+$(function() {
     var app = $("#app");
-
-
+    function renderBookDetails(bookDetails, data) {
+        bookDetails
+            .empty()
+            .append("ID: " + data.id + "<br />")
+            .append("Autor: " + data.author + "<br />")
+            .append("ISBN: " + data.isbn + "<br />")
+            .append("Tytuł: " + data.title + "<br />")
+            .append("Wydawca: " + data.publisher + "<br />")
+            .append("Typ: " + data.type + "<br />");
+    }
     function renderBooks(books) {
-        books.forEach(function (book) {
+        books.forEach(function(book) {
             var bookElement = $("<div>");
-            bookElement
-                .addClass("book")
-                .appendTo(app);
-
             var bookTitle = $("<div>");
+            var bookDetails = $("<div>");
+            bookElement.addClass("book").appendTo(app);
             bookTitle
                 .addClass("book-title")
                 .text(book.title)
                 .appendTo(bookElement);
-
-            var bookDetails = $("<div>");
-            bookDetails
-                .addClass("book-details")
-                .appendTo(bookElement);
+            bookTitle.one("click", function() {
+                bookDetails.text("...");
+                $.ajax({
+                    method: "GET",
+                    url: "http://localhost:8282/books/" + book.id
+                }).done(function(data) {
+                    renderBookDetails(bookDetails, data);
+                });
+            });
+            bookDetails.addClass("book-details").appendTo(bookElement);
         });
     }
-
-
+    function fetchBooks() {
         $.ajax({
-            url: 'http://localhost:8282/books/',
-            method: "GET"
+            method: "GET",
+            url: "http://localhost:8282/books"
         }).done(renderBooks);
-
+    }
+    fetchBooks();
 });
